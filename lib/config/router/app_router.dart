d6f118e23291489d774471362076044fbf867281
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-12-09 17:56:43 -0300

    ADD: movie screen and movie screen on router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -9,5 +9,13 @@ final appRouter = GoRouter(
       name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
     ),
+    GoRoute(
+      path: '/movie/:id',
+      name: MovieScreen.name,
+      builder: (context, state) {
+        final movieID = state.pathParameters['id'] ?? 'no-id';
+        return MovieScreen(movieId: movieID);
+      },
+    )
   ],
 );
