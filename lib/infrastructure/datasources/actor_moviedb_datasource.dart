2aad4a2d286a4d5977e05ac8ea14fefa86958aae
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-12-12 22:54:37 -0300

    ADD: actor repository impl

--- lib/infrastructure/datasources/actor_moviedb_datasource.dart
+++ lib/infrastructure/datasources/actor_moviedb_datasource.dart
@@ -1,11 +1,13 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/entities/actor.dart';
 import 'package:cinemapedia/domain/datasources/actors_datasource.dart';
+import 'package:cinemapedia/infrastructure/mappers/actor_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/credits_response.dart';
 import 'package:dio/dio.dart';
 
 class ActorMovieDbDatasource extends ActorsDatasource {
   final dio = Dio(BaseOptions(
-    baseUrl: '',
+    baseUrl: 'https://api.themoviedb.org/3',
     queryParameters: {
       'api_key': Environment.theMovieDbKey,
       'language': 'es-MX',
@@ -14,6 +16,14 @@ class ActorMovieDbDatasource extends ActorsDatasource {
 
   @override
   Future<List<Actor>> getActorsByMovie(String movieId) async {
-    return [];
+    final response = await dio.get('/movie/$movieId/credits');
+
+    final castResponse = CastResponse.fromJson(response.data);
+
+    List<Actor> actors = castResponse.cast
+        .map((cast) => ActorMapper.castToEntity(cast))
+        .toList();
+
+    return actors;
   }
 }
