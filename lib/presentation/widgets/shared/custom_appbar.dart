86ab566cbcbc07a681293fec39b998ef6de08b51
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-12-19 22:55:46 -0300

    ADD: search delegate on custom appbar

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppbar extends StatelessWidget {
@@ -27,7 +28,12 @@ class CustomAppbar extends StatelessWidget {
               ),
               const Spacer(),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  showSearch(
+                    context: context,
+                    delegate: SearchMovieDelegate(),
+                  );
+                },
                 icon: const Icon(Icons.search),
               )
             ],
