9d06c69807f665844086c210fe19cfc79fa552e5
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2023-12-26 17:59:03 -0300

    REFAC: _CustomGradient

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -95,31 +95,33 @@ class _CustomSliverAppBar extends StatelessWidget {
                 },
               ),
             ),
-            const SizedBox.expand(
-              child: DecoratedBox(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                stops: [0.7, 1.0],
-                colors: [
-                  Colors.transparent,
-                  Colors.black87,
-                ],
-              ))),
+            const _CustomGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.0, 0.2],
+              colors: [
+                Colors.transparent,
+                Colors.black87,
+              ],
             ),
-            const SizedBox.expand(
-              child: DecoratedBox(
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                stops: [0.0, 0.4],
-                colors: [
-                  Colors.black87,
-                  Colors.transparent,
-                ],
-              ))),
-            )
+            const _CustomGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.8, 1.0],
+              colors: [
+                Colors.transparent,
+                Colors.black54,
+              ],
+            ),
+            const _CustomGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.0, 0.3],
+              colors: [
+                Colors.transparent,
+                Colors.black87,
+              ],
+            ),
           ],
         ),
       ),
@@ -245,5 +247,32 @@ class _ActorsByMovie extends ConsumerWidget {
         },
       ),
     );
+  }
+}
+
+class _CustomGradient extends StatelessWidget {
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final List<double> stops;
+  final List<Color> colors;
+
+  const _CustomGradient(
+      {this.begin = Alignment.centerLeft,
+      this.end = Alignment.centerRight,
+      required this.stops,
+      required this.colors});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+        child: DecoratedBox(
+            decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: begin,
+        end: end,
+        stops: stops,
+        colors: colors,
+      ),
+    )));
   }
 }
